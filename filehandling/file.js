a1abbc29d2634c17b6d9a7c264e2cf66d6e59d61
Mubashir Ejaz <mubashirejazkhan@gmail.com>
2026-09-04 08:22:39 -0700

    read file

--- filehandling/file.js
+++ filehandling/file.js
@@ -2,3 +2,14 @@ const fs=require("fs")
 
 fs.writeFileSync(".test.txt","hi there")
 
+const result=fs.readFileSync("./contact.txt","UTF-8")
+console.log(result)
+
+fs.readFile=fs.readFile("./contact.txt","UTF-8",(err,result)=>{
+
+    if(err){
+        console.log("error",err)
+    }else{
+        console.log(result)
+    }
+})
