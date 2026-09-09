94e45df725b978ea3066c84cd8f4e8c9e2884b08
Mubashir Ejaz <mubashirejazkhan@gmail.com>
2026-09-09 07:53:38 -0700

    update

--- commonjs/app.js
+++ commonjs/app.js
@@ -1,9 +1,4 @@
-const maths=require("./maths")
+const maths=require("./cal.js")
 
 console.log(maths.add(10,10))
 console.log(maths.sub(20,10))
-
-import {mul,div} from "./maths.js"
-
-console.log(mul(2,2))
-console.log(div(2,2))
