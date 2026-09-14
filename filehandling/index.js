9f7d6dc3fd5eda657a60e869a2d03506be8e93d1
Mubashir Ejaz <mubashirejazkhan@gmail.com>
2026-09-14 08:08:56 -0700

    http server

---
+++ filehandling/index.js
@@ -0,0 +1,9 @@
+const http= require("http");
+
+const myserver=http.createServer((req,res)=>{
+
+    console.log("new req rec")
+    res.end("HELLO")
+});
+
+myserver.listen(8000,()=>console.log("server started"));
