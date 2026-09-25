382d5cb63dc28486ea016ced768c40336e8f73f1
Mubashir Ejaz <mubashirejazkhan@gmail.com>
2026-09-25 06:08:32 -0700

    post api

--- restapis/index.js
+++ restapis/index.js
@@ -3,6 +3,7 @@ const users=require("./MOCK_DATA.json")
 
 const app=express();
 const port=3000;
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/users", (req, res) => {
     const html =
@@ -13,27 +14,32 @@ app.get("/users", (req, res) => {
     res.send(html);
 });
 
+
+
 app.get("/api/users",(req,res)=>{
     res.json(users)
 })
-app.get("/api/users/:id", (req, res) => {
-    const id = Number(req.params.id);
-     const user = users.find((user) => user.id === id);
-       res.json(user);
 
-});
-app.post("/api/users",(req,res)=>{
-    //Create new users
-    res.json({status:"pending"});
-    
-app.patch("/api/users:id",(req,res)=>{
+app.route("/api/users/:id")
+.get((req,res)=>{
+    const id=Number(req.params.id);
+    const user=users.find((user)=>user.id===id);
+    res.json(user);
+})
+
+.patch((req,res)=>{
     //edit users
     res.json({status:"pending"});
-
-      
-app.delete("/api/users:id",(req,res)=>{
-    //remove user 
+})
+.delete((req,res)=>{
+    //remove user
     res.json({status:"pending"});
-
+});
+app.post("/api/users",(req,res)=>{
+    const body=req.body;
+    console.log("body",body)
+    
+    res.json({status:"pending"});
+})
 
 app.listen(port , ()=>console.log(`server start at ${port}`))
